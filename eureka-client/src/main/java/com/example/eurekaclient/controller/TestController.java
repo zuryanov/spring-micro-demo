ed6d44e8de85f062041c3c871248e98b65948e38
ZyryanovAD <zyryanovad@skblab.ru>
2022-10-21 02:23:53 +0600

    create property's repository

--- eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
+++ eureka-client/src/main/java/com/example/eurekaclient/controller/TestController.java
@@ -1,2 +1,22 @@
-package com.example.eurekaclient.controller;public class TestController {
+package com.example.eurekaclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/main")
+public class TestController {
+
+    @Value("${eureka.instance.instance-id}")
+    private String id;
+
+    @Value("${tmpVar}")
+    private int tmpVar;
+
+    @GetMapping("/test")
+    public String test() {
+        return "test " + id + " : " + tmpVar;
+    }
 }
